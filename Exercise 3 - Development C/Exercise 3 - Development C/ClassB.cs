e1ae4100cce525d1f9e9c68b9e57e95dde21d7d4
Evelyne Franco <evelyne.franco@trustvesta.com>
2019-04-13 00:25:36 -0500

    add exercise3

---
+++ Exercise 3 - Development C/Exercise 3 - Development C/ClassB.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3___Development_C
+{
+    class ClassB : ParentA, interfaceClass 
+    {
+        
+        static void Main(string[] args)
+        {
+            ParentA pa = new ParentA();
+            ClassB cb = new ClassB();
+            //use method compare strings from ParentA
+            Console.WriteLine("when words lenght is the same: ....");
+            pa.comparesLenghtString("thesame", "thesame");
+            Console.WriteLine("when words lenght is different: .....");
+            pa.comparesLenghtString("thesame", "not");
+            Console.WriteLine("---------------------------------");
+
+            //use method  compare lists from parentA
+            Console.WriteLine("Compare list size");
+
+            List<int> numeros = new List<int> { 1, 1, 2, 3, 4, 4, 5, 6, 7, 8, 10 };
+            List<string> values = new List<string> { "Text", "Computer", "Automation", "Visual Studio", "Auto Parts", " ", "Test" };
+            pa.compareLengthList(numeros, values);
+            Console.WriteLine("---------------------------------");
+
+            //method form int
+            cb.print();
+            Console.WriteLine("---------------------------------");
+
+            //pint list in asc/desc
+
+            Console.WriteLine("--------asc-------");
+            cb.ascDesc("asc", values);
+            Console.WriteLine("--------desc-------");
+            cb.ascDesc("desc", values);
+            Console.WriteLine("---------------------------------");
+
+            Console.WriteLine("------print list without space------");
+            //print list without space element
+            cb.skipSpace(values);
+            Console.WriteLine("---------------------------------");
+
+            Console.WriteLine("------Delete duplicates------");
+            cb.removeDuplicates(numeros);
+            Console.WriteLine("---------------------------------");
+        }
+
+        public void skipSpace(List<string> list)
+        {
+
+            IEnumerable<string> space = from a in list
+                                        where  a != " "
+                                        select a;
+            foreach (var result in space) {
+                Console.WriteLine(result);
+            }
+        }
+
+        public void removeDuplicates(List<int> list) {
+            List<int> duplicates = list.Distinct().ToList();
+
+            foreach (var dup in duplicates) {
+                Console.WriteLine(dup);
+            }
+
+
+        }
+
+        public void print()
+        {
+            Console.WriteLine("Hello Interface");
+        }
+
+        public void ascDesc(string order, List<string> list) {
+            list.Sort();
+
+            if (order.Equals("asc"))
+            {
+                foreach (string value in list)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+            else if (order.Equals("desc"))
+            {
+                list.Reverse();
+                foreach (string value in list)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+                
+            
+        }
+    }
+}
---
+++ Exercise 3 - Development C/Exercise 3 - Development C/ParentA.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3___Development_C
+{
+    class ParentA
+    {
+        public void comparesLenghtString(string word1, string word2) {
+            try
+            {
+                if (word1.Length == word2.Length)
+                {
+                    Console.WriteLine("Words lenght is the same");
+                }
+                else {
+                    Console.WriteLine("Words Lenht is different");
+                }
+            }
+            catch (NullReferenceException e) {
+                Console.WriteLine(e.Message);
+            }
+        }//method compare string
+
+        public void compareLengthList(List<int> list1, List<string> list2)
+        {
+            
+            try {
+                if (list1.Count() == list2.Count())
+                { //to acces to size list is with count...size is not displayed in menu options, addin size shows an error
+                    Console.WriteLine("list size is the same");
+                }
+                else {
+                    Console.WriteLine("List size is different");
+                }
+            }
+            catch (NullReferenceException e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
